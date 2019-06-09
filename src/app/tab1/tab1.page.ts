6c2b0bc96771d6c05e58e49062ae919ca1bad2b7
Simon Misrahi <simon@Simons-MacBook-Pro-3.local>
2019-06-09 23:48:04 +0100

    first commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {NavController} from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,13 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
-
-}
+  constructor(
+    private navCtrl: NavController
+    ) {}
+    
+    ngOnInit() {
+    }
+    navToLogin() {
+      this.navCtrl.navigateForward('');
+    }
+  }
